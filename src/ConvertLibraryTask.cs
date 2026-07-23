4837e00687e810868163550450cdb81e35b74f73
Katie Kloss <katherine.kloss@proton.me>
2026-07-23 12:15:03 -0400

    feat: enough to set a breakpoint

--- src/ConvertLibraryTask.cs
+++ src/ConvertLibraryTask.cs
@@ -1,6 +1,14 @@
+using Jellyfin.Data.Enums;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Persistence;
 using MediaBrowser.Model.Tasks;
 
-public class ConvertLibraryTask : IScheduledTask
+public class ConvertLibraryTask(IPluginManager _pluginManager,
+                                ILibraryManager _libraryManager,
+                                IItemRepository _itemRepo)
+    : IScheduledTask
 {
     public string Name => "Convert Dolby Vision MKVs";
 
@@ -14,6 +22,16 @@ public class ConvertLibraryTask : IScheduledTask
 
     public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var configuration = (_pluginManager.GetPlugin(Plugin.OurGuid)?.Instance as Plugin)?.Configuration
+            ?? throw new Exception("Can't get plugin configuration");
+
+        var allVideo = _itemRepo.GetItems(new InternalItemsQuery
+        {
+            MediaTypes = [MediaType.Video]
+        })
+        .Items
+        .Cast<Video>();
+
+        return Task.CompletedTask;
     }
 }
